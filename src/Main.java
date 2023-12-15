1e5cbe14c9e80403d0b0d54e01033bb86aaba314
Furkan Can Buyur <furkanbuyur@gmail.com>
2023-12-15 21:06:03 +0300

    startPrice added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         double km,azami = 20 ,startPrice = 10 ;
 
-
         Scanner inp = new Scanner(System.in);
         System.out.print("Gidilecek Mesafe (km) :...");
         km = inp.nextDouble();
