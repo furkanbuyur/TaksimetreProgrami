4e83b7b6fd01f7f156061cbca45bc1a539d5f04b
Furkan Can Buyur <furkanbuyur@gmail.com>
2023-12-15 21:07:39 +0300

    startPrice added

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        double km,azami = 20 ,startPrice = 10 ;
+        double tutar,km,azami = 20 ,startPrice = 10 ;
+
 
         Scanner inp = new Scanner(System.in);
         System.out.print("Gidilecek Mesafe (km) :...");
         km = inp.nextDouble();
-        float tutar = (float) (km * 2.2);
+        tutar = (km * 2.2);
         tutar += startPrice;
         tutar = tutar <= azami ? (float) azami : tutar;
         System.out.println("Taksimetre Tutarı: " + tutar + " TL" );
